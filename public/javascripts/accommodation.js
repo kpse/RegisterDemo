97076b1760c3833ba28847b1a1dc0bb2da18a66e
Suo Qin <kpsesuo@gmail.com>
2013-11-17 18:39:10 +0800

    add choosing accommodations

---
+++ public/javascripts/accommodation.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 11/17/13
+ * Time: 6:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,52 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module("app", ['ui.router']);
 
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            template: '<div>welcome to student registration</div>'
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('individual', {
+            url: '/individual',
+            templateUrl: 'templates/app.html'
         })
-        .when('/pizza', {
-            template: '<div class="button" pizza>Click me!</div>'
+        .state('accommodation', {
+            url: '/accommodation',
+            templateUrl: 'templates/list.html',
+            controller: 'ListCtrl'
         })
-        .when('/:message', {
-            templateUrl: 'app.html',
-            controller: 'AppCtrl'
-        })
-        .otherwise({
-            template: '404'
+        .state('accommodation.item', {
+            url: '/:item',
+            templateUrl: 'templates/list.item.html',
+            controller: function ($scope, $stateParams) {
+                $scope.item = $stateParams.item;
+                $scope.image = app.imageOf($stateParams.item);
+            }
         })
 })
 
-app.controller("AppCtrl", function ($scope, $routeParams) {
-    $scope.model = {
-        message: "this is my app! " + $routeParams.message
-    }
+app.imageOf = function (item) {
+    return {
+        'Milk': 'http://www.kremslehnerhotels.at/files/kremslehner-hotels/hotel-regina-kremslehner-vienna/2-rooms/superior/hotel-regina-superior-rooms-vienna.jpg',
+        'Eggs': 'http://www.automation-drive.com/EX/05-14-08/Accommodation.jpg',
+        'Bread': 'http://www.byron-bay.com.au/accommodation.JPG',
+        'Cheese': 'http://www.settlersvillage.co.za/accommodation2b.jpg',
+        'Ham': 'http://www.varianostravel.com/images/Phoenician_accommodation.jpg'
+    }[item];
+};
+
+app.controller('ListCtrl', function ($scope) {
+    $scope.shoppingList = [
+        {name: 'Milk'},
+        {name: 'Eggs'},
+        {name: 'Bread'},
+        {name: 'Cheese'},
+        {name: 'Ham'}
+    ];
+
+    $scope.selectItem = function (selectedItem) {
+        _($scope.shoppingList).each(function (item) {
+            item.selected = false;
+            if (selectedItem === item) {
+                selectedItem.selected = true;
+            }
+        });
+    };
 })
 
-
-
-app.directive("pizza", function () {
-    return function (scope, element) {
-        element.bind("click", function () {
-            alert("clicked!");
-        })
-    }
-})
