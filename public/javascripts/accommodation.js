28e1cacfd19090e6744974fcfef677419a3eb73f
Suo Qin <kpsesuo@gmail.com>
2013-11-17 19:25:13 +0800

    add wip page as default router

--- public/javascripts/accommodation.js
+++ public/javascripts/accommodation.js
@@ -1,7 +1,34 @@
-/**
- * Created with IntelliJ IDEA.
- * User: twer
- * Date: 11/17/13
- * Time: 6:32 PM
- * To change this template use File | Settings | File Templates.
- */
+
+app.imageOf = function (item) {
+    return {
+        'Milk': 'http://www.kremslehnerhotels.at/files/kremslehner-hotels/hotel-regina-kremslehner-vienna/2-rooms/superior/hotel-regina-superior-rooms-vienna.jpg',
+        'Eggs': 'http://www.automation-drive.com/EX/05-14-08/Accommodation.jpg',
+        'Bread': 'http://www.byron-bay.com.au/accommodation.JPG',
+        'Cheese': 'http://www.settlersvillage.co.za/accommodation2b.jpg',
+        'Ham': 'http://www.varianostravel.com/images/Phoenician_accommodation.jpg'
+    }[item];
+};
+
+app.controller('AccommodationCtrl', function ($scope) {
+    $scope.shoppingList = [
+        {name: 'Milk', desc: '宿舍1'},
+        {name: 'Eggs', desc: '宿舍2'},
+        {name: 'Bread', desc: '宿舍3'},
+        {name: 'Cheese', desc: '宿舍4'},
+        {name: 'Ham', desc: '宿舍5'}
+    ];
+
+    $scope.selectItem = function (selectedItem) {
+        _($scope.shoppingList).each(function (item) {
+            item.selected = false;
+            if (selectedItem === item) {
+                selectedItem.selected = true;
+            }
+        });
+    };
+})
+
+app.controller('SingleBuildingCtrl', function ($scope, $stateParams) {
+    $scope.item = $stateParams.item;
+    $scope.image = app.imageOf($stateParams.item);
+})
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,44 +9,19 @@ app.config(function ($stateProvider, $urlRouterProvider) {
         .state('accommodation', {
             url: '/accommodation',
             templateUrl: 'templates/list.html',
-            controller: 'ListCtrl'
+            controller: 'AccommodationCtrl'
         })
         .state('accommodation.item', {
             url: '/:item',
-            templateUrl: 'templates/list.item.html',
-            controller: function ($scope, $stateParams) {
-                $scope.item = $stateParams.item;
-                $scope.image = app.imageOf($stateParams.item);
-            }
+            templateUrl: 'templates/accommodation.building.html',
+            controller: 'SingleBuildingCtrl'
         })
+        .state('wip', {
+            url: '/wip',
+            template: '<div>Sorry, we are still in Building...</div>'
+        })
+
+    $urlRouterProvider.otherwise('/wip')
 })
 
-app.imageOf = function (item) {
-    return {
-        'Milk': 'http://www.kremslehnerhotels.at/files/kremslehner-hotels/hotel-regina-kremslehner-vienna/2-rooms/superior/hotel-regina-superior-rooms-vienna.jpg',
-        'Eggs': 'http://www.automation-drive.com/EX/05-14-08/Accommodation.jpg',
-        'Bread': 'http://www.byron-bay.com.au/accommodation.JPG',
-        'Cheese': 'http://www.settlersvillage.co.za/accommodation2b.jpg',
-        'Ham': 'http://www.varianostravel.com/images/Phoenician_accommodation.jpg'
-    }[item];
-};
-
-app.controller('ListCtrl', function ($scope) {
-    $scope.shoppingList = [
-        {name: 'Milk'},
-        {name: 'Eggs'},
-        {name: 'Bread'},
-        {name: 'Cheese'},
-        {name: 'Ham'}
-    ];
-
-    $scope.selectItem = function (selectedItem) {
-        _($scope.shoppingList).each(function (item) {
-            item.selected = false;
-            if (selectedItem === item) {
-                selectedItem.selected = true;
-            }
-        });
-    };
-})
 
