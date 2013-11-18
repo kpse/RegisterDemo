f86990124fced8da6da22429a7dcd7ed843724b8
Suo Qin <kpsesuo@gmail.com>
2013-11-18 16:16:56 +0800

    add jasmine test

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,12 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.gu.SbtJasminePlugin._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "registerDemo"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "registerDemo"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -15,8 +16,29 @@ object ApplicationBuild extends Build {
   )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  val main = play.Project(appName, appVersion, appDependencies)
+    .settings(jasmineSettings: _*)
+    .settings(// jasmine configuration overridden
+    appJsDir <+= sourceDirectory {
+      src => src  / ".." / "public" / "javascripts"
+    },
+
+    appJsLibDir <+= sourceDirectory {
+      src => src  / ".." / "public" / "javascripts" / "vendor" / "angularjs"
+    },
+
+    jasmineTestDir <+= sourceDirectory {
+      src => src / ".." / "test" / "assets"
+    },
+
+    jasmineConfFile <+= sourceDirectory {
+      src => src / ".." / "test" / "assets" / "test.dependencies.js"
+    },
+
+    // link jasmine to the standard 'sbt test' action.
+    // Now when running 'test' jasmine tests will be run
+    // and after that other Play tests will be executed.
+    (test in Test) <<= (test in Test) dependsOn (jasmine)
   )
 
 }
